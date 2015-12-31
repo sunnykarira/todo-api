abadae7b2162096b09ca28f12a11f7585650b1c0
sunnykarira <sunnykarira@outlook.com>
2016-01-01 05:06:11 +0530

    Refractor server

--- server.js
+++ server.js
@@ -1,105 +1,39 @@
- /* Basic Express Server */
-// var express = require('express');
-// var app = express();
+// Creating new todos
 
-// var PORT = process.env.PORT || 3000;
-
-// app.get('/', function(req, res){
-// 	res.send('Todo API Root');
-// });
-
-// app.listen(PORT, function(){
-// 	console.log('Express listening on port ' + PORT + '!');
-// });
-
-
-/* Getting all todos */
-
-// var express = require('express');
-// var app = express();
-// var PORT = process.env.PORT || 3000;
-// // All the todos will have description and completed.
-// // Multiple todo item is called todo collection.
-// var todos = [{
-// 	description: 'Meet for lunch',
-// 	completed: false,
-// 	id: 1
-// }, {
-// 	description: 'Go to market',
-// 	id: 2,
-// 	completed: false
-// }];
-
-// app.get('/', function(req, res){
-// 	res.send('Todo API Root');
-// });
-
-// // GET request /todos
-
-// app.get('/todos', function(req, res){
-// 	// res.json converts to json automatically
-// 	// no need of json.stringify
-// 	res.json(todos);
-// });
-
-// //GET individual todos  /todos/:id
-
-
-// app.listen(PORT, function(){
-// 	console.log('Express server listening at ' + PORT);
-// });
-
-// Challenge
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos =  [{
-	id: 1,
-	description: 'yoyo 1',
-	completed:  false
-}, {
-	id: 2,
-	description: 'yoyo 2',
-	completed: false
-},{
-	id: 3,
-	description: 'yoyo 3',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos
-app.get('/todos', function(req, res){
-	res.json(todos);
+app.get('/todos', function (req, res){
+	res.send(todos);
 });
 
-//GET /todos/:id
-app.get('/todos/:id', function(req, res){
-	//res.send('Asking for todo with id of ' + req.params.id);
+app.get('/todos/:id', function (req, res){
 
-	// Now searching for id and then sending the json data
-	// id is string, params are always string
-	var todoId = parseInt(req.params.id, 10);
-	// converts string to number with base
-	var mathchedTodo;
+	var todoId = parseInt(req.params.id);
+	var matchedTodo;
 
 	todos.forEach(function(todo){
-		if(todoId === todo.id){
-			mathchedTodo = todo;
+		if(todo.id === todoId){
+			matchedTodo = todo;
 		}
 	});
 
-	if(mathchedTodo){
-		res.json(mathchedTodo);
+	if(matchedTodo){
+		res.json(matchedTodo);
 	}else{
 		res.status(404).send();
 	}
+
 });
 
 app.listen(PORT, function(){
-	console.log('Server running!');
-});
+	console.log('Server Running!');
+})
