8269f0758d4e4b0f597f76fda5ed5cf0b353d00f
sunnykarira <sunnykarira@outlook.com>
2016-01-01 14:06:59 +0530

    Add POST for todos

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // Creating new todos
 
 var express = require('express');
+
+// It is an express middleware
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
-var todoNextId = 1;
+var todoNextId = 1; 
+
+// It is a middleware and we are using it
+app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {
@@ -12,7 +19,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/todos', function (req, res){
-	res.send(todos);
+	res.json(todos);
 });
 
 app.get('/todos/:id', function (req, res){
@@ -34,6 +41,18 @@ app.get('/todos/:id', function (req, res){
 
 });
 
+// POST /todos
+// post can take data with the request
+// npm install body-parser@1.13.3 --save
+app.post('/todos', function (req, res){
+
+	var body = req.body;
+	body.id = todoNextId;
+	todoNextId++;
+	todos.push(body);
+	res.json(body);
+});
+
 app.listen(PORT, function(){
 	console.log('Server Running!');
-})
+});
