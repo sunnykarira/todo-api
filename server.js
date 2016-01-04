d96aaa1fa30de442da0931bb55d2656388c98d4f
sunnykarira <sunnykarira@outlook.com>
2016-01-05 01:50:46 +0530

    Convert DELETE /todos to sequelize

--- server.js
+++ server.js
@@ -69,18 +69,20 @@ app.get('/todos/:id', function(req, res) {
 app.delete('/todos/:id', function(req, res) {
 
 	var todoId = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	db.todo.destroy({
+		where:{
+			id: todoId
+		}
+	}).then(function(rowsDeleted){
+		if(rowsDeleted === 0){
+			res.status(404).json({"error": "todo not found with id"});
+		}else{
+			// Everything went well and nothing  (data) is coming back
+			res.status(204).send();
+		}
+
 	});
 
-	if (!matchedTodo) {
-		return res.status(404).send({
-			"error": "todo not found"
-		});
-	}
-
-	todos = _.without(todos, matchedTodo);
-	res.json(todos);
 });
 
 app.post('/todos', function(req, res) {
